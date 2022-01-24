801f17f645741872f9f43d6f331cadde8168ab73
svante-jonsson <77492340+svante-jonsson@users.noreply.github.com>
2022-01-24 11:10:26 +0100

    configured dotenv

--- backend/main.js
+++ backend/main.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const app = express();
 const port = 3000;
+const history = require('connect-history-api-fallback');
+const dotenv = require('dotenv');
+require('dotenv').config();
 
-app.get('/api/', (req, res) => res.send('Hello World!'));
+app.get('/api/', (req, res) => res.send(`${process.env.TEST}`));
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
