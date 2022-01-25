c58bd5c721016cdeb14858bfb8549fb143e52b11
svante-jonsson <77492340+svante-jonsson@users.noreply.github.com>
2022-01-25 10:56:50 +0100

    PWA init

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,67 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from 'vite';
+import { VitePWA } from 'vite-plugin-pwa';
+import vue from '@vitejs/plugin-vue';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()]
-})
+	plugins: [
+		VitePWA({
+			manifest: {
+				background_color: 'grey',
+				display: 'standalone',
+				icons: [
+					{
+						purpose: 'any',
+						sizes: '192x192',
+						src: 'manifest-icon-192.maskable.png',
+						type: 'image/png'
+					},
+					{
+						purpose: 'maskable',
+						sizes: '192x192',
+						src: 'manifest-icon-192.maskable.png',
+						type: 'image/png'
+					},
+					{
+						purpose: 'any',
+						sizes: '512x512',
+						src: 'manifest-icon-512.maskable.png',
+						type: 'image/png'
+					},
+					{
+						purpose: 'maskable',
+						sizes: '512x512',
+						src: 'manifest-icon-512.maskable.png',
+						type: 'image/png'
+					}
+				],
+				name: 'Testapplikation',
+				short_name: 'Testapp',
+				start_url: '.',
+				theme_color: 'red'
+			},
+			registerType: 'autoUpdate',
+			workbox: {
+				globPatterns: [
+					'assets/**',
+					'index.html',
+					'manifest.webmanifest'
+				],
+				runtimeCaching: [
+					{
+						handler: 'NetworkFirst',
+						// Cache GET requests to https://avancera.app/cities/
+						urlPattern: '/api/'
+					},
+					{
+						handler: 'NetworkFirst',
+						// Regular expression to cache GET requests to
+						// “https://avancera.app/cities/ID” (with or without a trailing
+						// slash)
+						urlPattern: new RegExp('/api/[0-9a-f-]+/?')
+					}
+				]
+			}
+		}),
+		vue()
+	]
+});
