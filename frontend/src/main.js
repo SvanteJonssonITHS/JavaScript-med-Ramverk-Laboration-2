0f05c1918b575a64b4c84a0e70a8ccca323be563
svante-jonsson <77492340+svante-jonsson@users.noreply.github.com>
2022-01-25 11:35:26 +0100

    Sass init

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -2,7 +2,7 @@ import { createApp } from 'vue';
 import App from './App.vue';
 import router from './router';
 import store from './store';
-import './style.css';
+import './style.scss';
 import axios from 'axios';
 import VueAxios from 'vue-axios';
 
