fc597f7c1da185e73f9003b6f3f1cb956520a0af
svante-jonsson <77492340+svante-jonsson@users.noreply.github.com>
2022-01-26 19:25:51 +0100

    Create Navbar.vue

---
+++ frontend/src/components/Navbar.vue
@@ -0,0 +1,29 @@
+<script>
+	import Search from './Search.vue';
+
+	export default {
+		components: { Search },
+		data() {
+			return {
+				greeting: 'Hello World!'
+			};
+		}
+	};
+</script>
+
+<template>
+	<nav class="bg-black p-3">
+		<Search />
+		<router-link
+			to=""
+			class="inline-flex items-center px-3 py-2 font-medium rounded px-4 py-2 leading-5 bg-green-500 text-primary-100 text-white hover:text-white hover:bg-green-700"
+		>
+			<span class="material-icons inline-block w-5 h-5 mr-2">
+				favorite_border
+			</span>
+			My favorites
+		</router-link>
+	</nav>
+</template>
+
+<style scoped></style>
