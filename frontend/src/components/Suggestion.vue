0c607ef03561129b0077f2fab4d53c86abc09010
svante-jonsson <77492340+svante-jonsson@users.noreply.github.com>
2022-01-26 19:24:37 +0100

    Create Suggestion.vue

---
+++ frontend/src/components/Suggestion.vue
@@ -0,0 +1,36 @@
+<script>
+	export default {
+		props: {
+			title: {
+				type: String,
+				required: true
+			},
+			year: {
+				type: String,
+				required: true
+			},
+			posterURL: {
+				type: String,
+				required: true
+			}
+		}
+	};
+</script>
+
+<template>
+	<article class="flex h-full p-2">
+		<section class="h-full">
+			<img
+				:src="this.posterURL"
+				:alt="this.title + ' poster'"
+				class="h-full"
+			/>
+		</section>
+		<section class="pl-2 font-semibold">
+			<h1 class="text-xl">{{ this.title }}</h1>
+			<p class="text-sm opacity-75">{{ this.year }}</p>
+		</section>
+	</article>
+</template>
+
+<style scoped></style>
