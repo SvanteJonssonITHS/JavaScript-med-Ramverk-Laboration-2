d40370b8b0215c97ab0941afc6a66c85c97700a9
svante-jonsson <77492340+svante-jonsson@users.noreply.github.com>
2022-01-26 22:57:46 +0100

    created /fetchResult route

--- backend/router.js
+++ backend/router.js
@@ -10,6 +10,19 @@ router.get('/fetchSuggestions/:query', async (req, res) => {
 		`http://www.omdbapi.com/?apikey=${process.env.APIKEY}&s=${req.params.query}`
 	);
 	res.send(response.data);
+
+router.get('/fetchResult/:query', async (req, res) => {
+	let response = await axios.get(
+		`http://www.omdbapi.com/?apikey=${process.env.APIKEY}&s=${req.params.query}`
+	);
+
+	const imdbID = response.data.Search[0].imdbID;
+
+	response = await axios.get(
+		`http://www.omdbapi.com/?apikey=${process.env.APIKEY}&i=${imdbID}&plot=full`
+	);
+
+	res.send(response.data).status(200);
 });
 
 module.exports = router;
