cf6c9b65918a6cecc8d5c3bbdb9b4ac61d547961
svante-jonsson <77492340+svante-jonsson@users.noreply.github.com>
2022-01-29 20:19:26 +0100

    basic structure for the results page

--- frontend/src/views/Results.vue
+++ frontend/src/views/Results.vue
@@ -1,9 +1,50 @@
 <template>
-	<p>{{ $route.params.query }}</p>
+	<main class="h-full">
+		<section class="py-4 w-4/5 mx-auto">
+			<h1 class="text-3xl">
+				<span class="font-bold">Search results for:</span> {{ query }}
+			</h1>
+		</section>
+		<section class="w-4/5 mx-auto">
+			<ResultsList>
+				<li v-for="result in results">
+					<router-link to="/">
+						<Result
+							class="h-24 w-full border-b-2 border-slate-300"
+							:title="result.Title"
+							:year="result.Year"
+							:posterURL="result.Poster"
+						/>
+					</router-link>
+				</li>
+			</ResultsList>
+		</section>
+	</main>
 </template>
 
 <script>
+	import ResultsList from '../components/ResultsList.vue';
+	import Result from '../components/Result.vue';
 	export default {
-		name: 'Results'
+		name: 'Results',
+		components: { ResultsList, Result },
+		data() {
+			return {
+				query: '',
+				results: {}
+			};
+		},
+		methods: {
+			async getResults(input) {
+				const response = await this.axios.get(
+					`/api/getResults/${input}`
+				);
+				this.results = response.data.Search;
+			}
+		},
+		created() {
+			this.query = this.$route.params.query;
+			this.getResults(this.query);
+		}
 	};
 </script>
