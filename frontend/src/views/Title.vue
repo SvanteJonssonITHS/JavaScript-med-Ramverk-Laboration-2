677b1d2322186e9b1ec4261121affaf2c6bbd1ae
svante-jonsson <77492340+svante-jonsson@users.noreply.github.com>
2022-01-29 20:46:06 +0100

    added a GET request for the title information

--- frontend/src/views/Title.vue
+++ frontend/src/views/Title.vue
@@ -1,9 +1,23 @@
 <template>
-	<main class="h-full"></main>
+	<main class="h-full">{{ title }}</main>
 </template>
 
 <script>
 	export default {
-		name: 'Title'
+		name: 'Title',
+		data() {
+			return {
+				title: {}
+			};
+		},
+		methods: {
+			async getTitle(input) {
+				const response = await this.axios.get(`/api/getTitle/${input}`);
+				this.title = response.data;
+			}
+		},
+		created() {
+			this.getTitle(this.$route.params.imdbID);
+		}
 	};
 </script>
