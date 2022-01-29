84784a365766731677da2c92c39f9607a2e2eed9
svante-jonsson <77492340+svante-jonsson@users.noreply.github.com>
2022-01-29 20:18:31 +0100

    Create ResultsList.vue

---
+++ frontend/src/components/ResultsList.vue
@@ -0,0 +1,14 @@
+<script>
+	export default {};
+</script>
+
+<template>
+	<ul>
+		<slot>
+			No results were found for:
+			<span class="italic">{{ $route.params.query }}</span>
+		</slot>
+	</ul>
+</template>
+
+<style scoped></style>
