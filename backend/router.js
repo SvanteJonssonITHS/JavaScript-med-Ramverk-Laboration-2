fa2dd8caa7aeed451c4d1d43f7e300a5c5bc41aa
svante-jonsson <77492340+svante-jonsson@users.noreply.github.com>
2022-02-01 14:45:42 +0100

    removed console.logs

--- backend/router.js
+++ backend/router.js
@@ -6,9 +6,6 @@ const axios = require('axios');
 const router = express.Router();
 
 router.get('/getResults/:query', async (req, res) => {
-	console.log('GET RESULTS', req.params.query);
-	// Add failsafes incase input is wrong
-	//Ability to get multiple pages
 	const response = await axios.get(
 		`http://www.omdbapi.com/?apikey=${process.env.APIKEY}&s=${req.params.query}`
 	);
@@ -16,9 +13,6 @@ router.get('/getResults/:query', async (req, res) => {
 });
 
 router.get('/getTitle/:imdbID', async (req, res) => {
-	console.log('GET TITLE', req.params.imdbID);
-
-	// Add failsafes incase input is wrong
 	response = await axios.get(
 		`http://www.omdbapi.com/?apikey=${process.env.APIKEY}&i=${req.params.imdbID}&plot=full`
 	);
