ab7eb8858fb8ef533c35e2f2456f247d5763040b
svante-jonsson <77492340+svante-jonsson@users.noreply.github.com>
2022-02-01 15:07:44 +0100

    added sortSearch component to favorites view

--- frontend/src/views/Favorites.vue
+++ frontend/src/views/Favorites.vue
@@ -4,6 +4,8 @@
 			<h1 class="text-3xl font-bold">My favorites</h1>
 		</section>
 		<section class="w-4/5 mx-auto flex flex-wrap">
+			<SortSearch @change-sort="changeSort" @search="newSearch" class="px-3" :show-best="false" />
+
 			<ResultsList v-if="favorites" class="w-full" no-result-message="No favorites were found!">
 				<li v-for="favorite in favorites" :key="favorite.imdbID">
 					<router-link :to="`/title/${favorite.imdbID}`">
@@ -21,15 +23,37 @@
 </template>
 
 <script>
+	import SortSearch from '../components/SortSearch.vue';
 	import ResultsList from '../components/ResultsList.vue';
 	import Result from '../components/Result.vue';
 	export default {
 		name: 'Favorites',
-		components: { ResultsList, Result },
+		components: { SortSearch, ResultsList, Result },
 		data() {
 			return {
 				favorites: null
 			};
+		},
+		methods: {
+			changeSort(typeOfSort, reverseSort) {
+				if (!this.shownResults) return;
+				let order = [...this.shownResults];
+				switch (typeOfSort) {
+					case 'abc':
+						order.sort((a, b) => a.Title.localeCompare(b.Title));
+						break;
+					case 'year':
+						order.sort((a, b) => parseInt(a.Year.substring(0, 4)) - parseInt(b.Year.substring(0, 4)));
+						break;
+					case 'type':
+						order.sort((a, b) => a.Type.localeCompare(b.Type));
+						break;
+					default:
+						order.sort((a, b) => a.Title.localeCompare(b.Title));
+						break;
+				}
+				this.shownResults = reverseSort ? order.reverse() : order;
+			}
 		},
 		created() {
 			this.favorites = localStorage.getItem('favorites') ? JSON.parse(localStorage.getItem('favorites')) : null;
