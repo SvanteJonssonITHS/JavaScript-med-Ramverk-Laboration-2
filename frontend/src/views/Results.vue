c448321690cb3f250339b0120bc7b2e798aa627a
svante-jonsson <77492340+svante-jonsson@users.noreply.github.com>
2022-02-01 11:08:49 +0100

    Update Results.vue

--- frontend/src/views/Results.vue
+++ frontend/src/views/Results.vue
@@ -1,13 +1,12 @@
 <template>
 	<main class="h-full">
 		<section class="py-4 w-4/5 mx-auto">
-			<h1 class="text-3xl">
-				<span class="font-bold">Search results for:</span> {{ query }}
-			</h1>
+			<h1 class="text-3xl"><span class="font-bold">Search results for:</span> {{ query }}</h1>
 		</section>
-		<section class="w-4/5 mx-auto">
-			<ResultsList>
-				<li v-for="result in results">
+		<section class="w-4/5 mx-auto flex flex-wrap">
+			<SortSearch @changeSort="changeSort" @search="updatePage" class="" />
+			<ResultsList class="">
+				<li v-for="result in shownResults">
 					<router-link :to="`/title/${result.imdbID}`">
 						<Result
 							class="h-24 w-full border-b-2 border-slate-300"
@@ -23,28 +22,60 @@
 </template>
 
 <script>
+	import SortSearch from '../components/SortSearch.vue';
 	import ResultsList from '../components/ResultsList.vue';
 	import Result from '../components/Result.vue';
 	export default {
 		name: 'Results',
-		components: { ResultsList, Result },
+		components: { SortSearch, ResultsList, Result },
 		data() {
 			return {
-				query: '',
-				results: {}
+				query: this.$route.params.query || '',
+				originalResults: {},
+				shownResults: {}
 			};
 		},
 		methods: {
 			async getResults(input) {
-				const response = await this.axios.get(
-					`/api/getResults/${input}`
-				);
-				this.results = response.data.Search;
+				const response = await this.axios.get(`/api/getResults/${input}`);
+				this.originalResults = response.data.Search;
+				this.shownResults = [...this.originalResults];
+			},
+			changeSort(typeOfSort, reverseSort) {
+				let order = [...this.shownResults];
+				switch (typeOfSort) {
+					case 'best':
+						order = [...this.originalResults];
+						break;
+					case 'abc':
+						order.sort((a, b) => a.Title.localeCompare(b.Title));
+						break;
+					case 'year':
+						order.sort((a, b) => a.Year.substring(1, 4).localeCompare(b.Year.substring(1, 4)));
+						break;
+					case 'type':
+						order.sort((a, b) => a.Type.localeCompare(b.Type));
+						break;
+					default:
+						order = [...this.originalResults];
+						break;
+				}
+				this.shownResults = reverseSort ? order.reverse() : order;
+			},
+			updatePage(input) {
+				this.$router.push(`/results/${input}`);
+				this.query = input;
+				this.getResults(input);
 			}
 		},
-		created() {
-			this.query = this.$route.params.query;
-			this.getResults(this.query);
+		async created() {
+			await this.getResults(this.query);
+			this.changeSort(this.$store.state.typeOfSort, this.$store.state.reverseSort);
+		},
+		watch: {
+			originalResults() {
+				this.changeSort(this.$store.state.typeOfSort, this.$store.state.reverseSort);
+			}
 		}
 	};
 </script>
